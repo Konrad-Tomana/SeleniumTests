12247b039b31064bb9f113a791c2533037047471
Konrad Tomana <tomana.konrad@gmail.com>
2020-03-04 17:47:59 +0100

    Quit and close method

--- src/test/java/openingTheBrowser.java
+++ src/test/java/openingTheBrowser.java
@@ -1,3 +1,5 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
@@ -8,8 +10,13 @@ public class openingTheBrowser {
     public void googleOpenTest(){
         String driverPath = "C:\\Users\\chesh\\IdeaProjects\\TestySelenium\\src\\main\\resources\\executables\\executables.drivers\\chromedriver.exe";
         System.setProperty("webdriver.chrome.driver", driverPath);
+        Dimension dimension = new Dimension(1300,800);
         WebDriver driver = new ChromeDriver();
-        driver.get("http://www.google.com");
+        driver.manage().window().setSize(dimension);
+        driver.get("D://CP//Files//test.html");
+        driver.findElement (By.id("newPage")).click();
+        driver.quit(); //zamyka wszystkie okna
+        //driver.close(); //zamyka pierwotne okno
 
     }
 }
