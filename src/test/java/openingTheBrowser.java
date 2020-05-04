4a57027daacc936135267d665756f973f11e854e
Konrad Tomana <tomana.konrad@gmail.com>
2020-05-04 11:29:34 +0200

    select by link text

--- src/test/java/openingTheBrowser.java
+++ src/test/java/openingTheBrowser.java
@@ -1,6 +1,6 @@
 import org.openqa.selenium.By;
-import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
@@ -10,13 +10,13 @@ public class openingTheBrowser {
     public void googleOpenTest(){
         String driverPath = "C:\\Users\\chesh\\IdeaProjects\\TestySelenium\\src\\main\\resources\\executables\\executables.drivers\\chromedriver.exe";
         System.setProperty("webdriver.chrome.driver", driverPath);
-        Dimension dimension = new Dimension(1300,800);
-        WebDriver driver = new ChromeDriver();
-        driver.manage().window().setSize(dimension);
+        WebDriver driver= new ChromeDriver();
+        driver.manage().window().maximize();
         driver.get("D://CP//Files//test.html");
-        driver.findElement (By.id("newPage")).click();
-        driver.quit(); //zamyka wszystkie okna
-        //driver.close(); //zamyka pierwotne okno
+        WebElement clickOnMeButton = driver.findElement(By.id("clickOnMe"));
+        WebElement firstNameInput = driver.findElement(By.name("fname"));
+        WebElement w3SchoolLink = driver.findElement(By.linkText("Visit W3Schools.com!"));
+        WebElement googleLink = driver.findElement(By.partialLinkText("Weird"));
 
     }
 }
