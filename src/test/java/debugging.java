f8c11b6d5d6fce0e8c6d993a6e5dbb5d1d728225
Konrad Tomana <tomana.konrad@gmail.com>
2020-05-04 15:45:27 +0200

    quick lesson about debugging

--- src/test/java/debugging.java
+++ src/test/java/debugging.java
@@ -2,32 +2,20 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.Test;
 
-public class checkboxAndRadiobuttons {
-
+public class debugging {
     @Test
-    public void googleOpenTest() throws InterruptedException {
+    public void googleOpenTest() {
         String driverPath = "C:\\Users\\chesh\\IdeaProjects\\TestySelenium\\src\\main\\resources\\executables\\executables.drivers\\chromedriver.exe";
         System.setProperty("webdriver.chrome.driver", driverPath);
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("D://CP//Files//test.html");
 
-
-        WebElement firstNameInput = driver.findElement(By.id("fname"));
-        Thread.sleep(2000);
-        firstNameInput.sendKeys("Konrad");
-        Thread.sleep(1000);
-        Select carSelect = new Select (driver.findElement(By.tagName("select")));
-        carSelect.selectByVisibleText("Mercedes");
-
         WebElement checkbox = driver.findElement(By.xpath("//input[@type='checkbox']"));
-        Thread.sleep(1000);
         checkbox.click();
         WebElement radiobutton = driver.findElement(By.xpath("//input[@value='male']"));
-        Thread.sleep(1000);
         radiobutton.click();
 
 
