33187b8bfe1a611606fd19290b392d28ba550d7f
Konrad Tomana <tomana.konrad@gmail.com>
2020-05-05 10:40:14 +0200

    use getAttribute string name "textcontent" for hidden text

--- src/test/java/hiddenText.java
+++ src/test/java/hiddenText.java
@@ -4,7 +4,8 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
-public class hiddenAtribute {
+public class hiddenText {
+
     @Test
     public void googleOpenTest() {
         String driverPath = "C:\\Users\\chesh\\IdeaProjects\\TestySelenium\\src\\main\\resources\\executables\\executables.drivers\\chromedriver.exe";
@@ -14,12 +15,11 @@ public class hiddenAtribute {
         driver.get("D://CP//Files//test.html");
 
         WebElement paragraph = driver.findElement(By.className("topSecret"));
-        System.out.println("mój tekst to: " + paragraph.getText()); // intuicyjnie nie podzialamy
-        System.out.println("mój tekst to: " + paragraph.getAttribute("value")); // tu też, zwróci nam null
-        System.out.println("mój tekst to: " + paragraph.getAttribute("textContent")); // use getAttribute string name "testcontent" for hidden text
-
-
-
+        System.out.println("Mój tekst to: " + paragraph.getText()); // intuicyjnie nie zadziala
+        System.out.println("Mój tekst to: " + paragraph.getAttribute("value")); // to też,
+        System.out.println("Mój tekst to: " + paragraph.getAttribute("textContent") + " Solution: for hidden text use getAttribute string name \"testContent\"");
+        // for hidden text use getAttribute string name "testContent"
+        driver.quit();
 
     }
 }
