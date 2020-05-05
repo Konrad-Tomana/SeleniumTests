bf6ce160be1bccab2e6e4dd17303e56586a70956
Konrad Tomana <tomana.konrad@gmail.com>
2020-05-05 13:01:59 +0200

    building method for searching items

--- src/test/java/existingItem.java
+++ src/test/java/existingItem.java
@@ -1,3 +1,5 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
@@ -10,8 +12,29 @@ public class existingItem {
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("D://CP//Files//test.html");
+        checkIfElementExist(By.tagName("a"),driver);
+        checkIfElementExist(By.tagName("aaaa"),driver);
 
-
-        //driver.quit();
+        checkIfElementExist(driver,By.tagName("a"));
+        checkIfElementExist(driver,By.tagName("aaaa"));
     }
+
+    public boolean checkIfElementExist (By locator,WebDriver driver)    {
+        if(driver.findElements(locator).size()>0) {
+            System.out.println("Element istnieje na stronie");
+            return true;
+        }
+        System.out.println("Brak elementu na stronie");
+        return false;
+    }
+
+    public void checkIfElementExist(WebDriver driver,By locator) {
+        try {
+            driver.findElement(locator);
+            System.out.println("Element istnieje na stronie");
+        }catch (NoSuchElementException exc) {
+            System.out.println("Element nie istnieje");
+        }
+    }
+
 }
